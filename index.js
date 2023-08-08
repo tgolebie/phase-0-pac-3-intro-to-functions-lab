988763d1ed926b0910bba1901a923ac166118e78
Tom Golebiewski <tgolebie16@gmail.com>
2023-08-07 23:37:23 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+     // todo
+  }
+  function whisper(string) {
+     return string;
+  } 
+  "hello".toLowerCase(); // "hello"
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase())
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+     return"I can\'t hear you!"
+        else if (string === 'HELLO')
+      return "YES INDEED!"
+       else (string === "Let/'s have dinner together!")
+       return "I would love to!"
+  }
